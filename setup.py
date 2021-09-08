597d5378b528dd1597096dc0b7a07c14eea103e2
gupta24 <rockrahul581@gmail.com>
2021-09-08 10:52:29 +0530

    new addition of init file

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
-import setuptools
+from setuptools import setup, find_packages 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+
+VERSION = '0.0.1'
+DESCRIPTION = 'for data authentication by middleware'
+LONG_DESCRIPTION = long_description
+
+
+
+setup(
     name='middleware_service',
-    version='0.0.1',
+    version=VERSION,
     author='Rahul',
     author_email='rahul.gupta@adcuratio.com',
-    description='for data authentication by middleware',
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url='https://github.com/gupta24/service',
     project_urls = {
         "Bug Tracker": "https://github.com/gupta24/service/issues"
     },
     license='MIT',
-    packages=setuptools.find_packages(include=['middleware_service', 'middleware_service.*', 'middleware_service.src.signatures', 'middleware_service.src.v1.*']),
+    packages=find_packages(),
     install_requires=[],
     include_package_data=True,
     zip_safe=False
