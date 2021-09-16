34f7c0c7d589215bd3c44fbe989f057699dceab0
gupta24 <rockrahul581@gmail.com>
2021-09-16 10:40:35 +0530

    changes

--- middleware_service/signatures/__init__.py
+++ middleware_service/signatures/__init__.py
@@ -1,14 +1,12 @@
 from .hmac_signature import hash_function as hash_function
 from .secret_key_generator import generate_secrete_key as generate_key
 from .signature_validation import check_signature as signature_validate
-from .secret_key_generator import set_secret_key
 
 __version__ = "0.1.0"
 __all__ = [
     'hash_function',
     'generate_key',
     'signature_validate',
-    'set_secret_key'
     ]
 
 
