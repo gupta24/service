208d7c14298ed4629aad6b4ee31fd2f8d7870e88
gupta24 <rockrahul581@gmail.com>
2021-09-17 15:17:18 +0530

    change key generator

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -1,11 +1,17 @@
-import secrets
+default_secret_key = b'(1O\x0b+0\xdd\xca8\x11\x9a\xcd\xe6\xd09c'
+input_secret_key = ''
+is_setup_key = False
 
-secret_key = b'(1O\x0b+0\xdd\xca8\x11\x9a\xcd\xe6\xd09c'
-def generate_secrete_key():
-    global secret_key
-    secret_key = secrets.token_bytes(16)
-    #print(secret_key)
+
+def generate_secret_key(input_byte_key):
+    global input_secret_key, is_setup_key
+    input_secret_key = input_byte_key
+    is_setup_key = True
+
     
 def get_secret_key():
-    return secret_key
+    global is_setup_key, default_secret_key, input_secret_key
+    if is_setup_key:
+        return input_secret_key
+    return default_secret_key
 
