52107f6db850c0af6a481fbff708b6af7e8158bf
gupta24 <35717122+gupta24@users.noreply.github.com>
2021-09-19 22:58:05 +0530

    Update secret_key_generator.py

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -4,18 +4,20 @@ is_setup_key = False
 
 
 def generate_secret_key(input_byte_key):
-    global input_secret_key, is_setup_key
+    global input_secret_key
+    global is_setup_key
     input_secret_key = input_byte_key
-    print(is_setup_key)
     is_setup_key = True
     print(is_setup_key)
 
     
 def get_secret_key():
-    global is_setup_key, default_secret_key, input_secret_key
-    print(is_setup_key)
+    global is_setup_key
+    global default_secret_key
+    global input_secret_key
     if is_setup_key:
         is_setup_key = False
+        print(is_setup_key)
         return input_secret_key
     return default_secret_key
 
