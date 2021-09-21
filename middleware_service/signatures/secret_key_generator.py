b9f240c85b8b6d91427b837c1cb8d71f98b08f9f
gupta24 <rockrahul581@gmail.com>
2021-09-21 09:16:40 +0530

    new cng

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -1,10 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 default_secret_key = b'(1O\x0b+0\xdd\xca8\x11\x9a\xcd\xe6\xd09c'
 input_secret_key = ''
 is_setup_key = False
 
-secret_key = os.environ.get('SECRET_KEY')
+secret_key = os.getenv('SECRET_KEY')
 
 def generate_secret_key(input_byte_key):
     global input_secret_key
@@ -20,6 +21,5 @@ def get_secret_key():
     if is_setup_key:
         is_setup_key = False
         return input_secret_key
-    print(secret_key)
-    return default_secret_key
+    return secret_key
 
