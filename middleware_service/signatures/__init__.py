a5d10b4e598744ed4e7205122a978c42213fb642
gupta24 <rockrahul581@gmail.com>
2021-09-21 15:13:24 +0530

    new changes

--- middleware_service/signatures/__init__.py
+++ middleware_service/signatures/__init__.py
@@ -1,11 +1,9 @@
 from .hmac_signature import hash_function as hash_function
-from .secret_key_generator import generate_secret_key as set_key
 from .signature_validation import check_signature as signature_validate
 
 __version__ = "0.1.0"
 __all__ = [
     'hash_function',
-    'set_key',
     'signature_validate'
     ]
 
--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -1,27 +1,24 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+
 
 default_secret_key = b'(1O\x0b+0\xdd\xca8\x11\x9a\xcd\xe6\xd09c'
-input_secret_key = ''
-is_setup_key = False
-
 load_dotenv()
-secret_key = os.getenv('SECRET_KEY')
 
-def generate_secret_key(input_byte_key):
-    global input_secret_key
-    global is_setup_key
-    input_secret_key = input_byte_key
-    is_setup_key = True
 
-    
+def KeyException(Exception):
+    pass
+
+
 def get_secret_key():
-    global is_setup_key
     global default_secret_key
-    global input_secret_key
-    if is_setup_key:
-        is_setup_key = False
-        return input_secret_key
+    print(find_dotenv())
+
+    try:
+        secret_key = os.getenv('SECRET_KEY')
+    except KeyError as e:
+        raise KeyException(f"SECRET_KEY does not exist in .env file, setup variable name in your .env")
+
     print(secret_key)
     return default_secret_key
 
