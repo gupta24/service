2d5009ef852abdd7c735eda752b0c8ebce771460
gupta24 <35717122+gupta24@users.noreply.github.com>
2021-09-22 11:01:45 +0530

    Update secret_key_generator.py

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -1,20 +1,15 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-
-default_secret_key = b'(1O\x0b+0\xdd\xca8\x11\x9a\xcd\xe6\xd09c'
-load_dotenv(find_dotenv())
-
+path = find_dotenv()
+load_dotenv(path)
 
 def KeyError(BaseException):
     pass
 
 
 def get_secret_key():
-    global default_secret_key
-
     try:
-        print(find_dotenv())
         secret_key = os.environ['SECRET_KEY']
         print(secret_key)
         secret_key = secret_key.encode('ascii')
