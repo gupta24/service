7716c04a0f412e63f790b4eb1143035106f8b642
gupta24 <35717122+gupta24@users.noreply.github.com>
2021-09-22 11:10:43 +0530

    Update secret_key_generator.py

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -10,9 +10,10 @@ load_dotenv(path)
 
 def get_secret_key():
 #    try:
+    print(path)
     secret_key = os.getenv('SECRET_KEY')
     print(secret_key)
-    secret_key = secret_key.encode('ascii')
+    #secret_key = secret_key.encode('ascii')
 #    except BaseException as e:
 #        raise KeyError("SECRET_KEY does not exist in .env file, setup variable name in your .env")
 
