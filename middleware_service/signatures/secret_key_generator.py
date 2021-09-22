103c7d2929a46e461769a53ff36cd6d15714013e
gupta24 <35717122+gupta24@users.noreply.github.com>
2021-09-22 12:57:48 +0530

    Update secret_key_generator.py

--- middleware_service/signatures/secret_key_generator.py
+++ middleware_service/signatures/secret_key_generator.py
@@ -1,16 +1,46 @@
 import os
 import sys
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
+from typing import (IO, Dict, Iterable, Iterator, Mapping, Optional, Tuple, Union)
 
 
-filename = '.env'
-frame = sys._getframe()
-# find first frame that is outside of this file
-while frame.f_code.co_filename == __file__:
-    frame = frame.f_back
-frame_filename = frame.f_code.co_filename
-dirname = os.path.dirname(os.path.abspath(frame_filename))
-filepath = os.path.join(dirname, filename)
+
+def _walk_to_root(path: str) -> Iterator[str]:
+    """
+    Yield directories starting from the given directory up to the root
+    """
+    if not os.path.exists(path):
+        raise IOError('Starting path not found')
+
+    if os.path.isfile(path):
+        path = os.path.dirname(path)
+
+    last_dir = None
+    current_dir = os.path.abspath(path)
+    while last_dir != current_dir:
+        yield current_dir
+        parent_dir = os.path.abspath(os.path.join(current_dir, os.path.pardir))
+        last_dir, current_dir = current_dir, parent_dir
+
+
+def find_env():
+    filename = '.env'
+    frame = sys._getframe()
+    # find first frame that is outside of this file
+    while frame.f_code.co_filename == __file__:
+        frame = frame.f_back
+    frame_filename = frame.f_code.co_filename
+    dirname = os.path.dirname(os.path.abspath(frame_filename))
+
+    for dirname in _walk_to_root(path):
+        check_path = os.path.join(dirname, filename)
+        if os.path.isfile(check_path):
+            return check_path
+    
+    return ''
+    
+#filepath = os.path.join(dirname, filename)
+filepath = find_env()
 load_dotenv(filepath)
 
 #def KeyError(BaseException):
@@ -22,7 +52,7 @@ def get_secret_key():
     print(filepath)
     secret_key = os.getenv('SECRET_KEY')
     print(secret_key)
-    #secret_key = secret_key.encode('ascii')
+    secret_key = secret_key.encode('ascii')
 #    except BaseException as e:
 #        raise KeyError("SECRET_KEY does not exist in .env file, setup variable name in your .env")
 
